8cdbf297cd49fbfdeba9cca4eeccfa89563b192a
luanafront <luana.lopes.rodrigues1@gmail.com>
2022-04-27 15:56:41 -0300

    personalizao e incluir section do js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ console.log(root)
 const cabecalho = document.createElement("header")
 cabecalho.classList.add("cabecalho")
 
-//funcao
-function criarElemento(elementoHTML , classe, conteudo){
+//funcao para atribuir elementos no Html
+function criarElemento(elementoHTML , classe = "", conteudo = ""){
     const elemento = document.createElement(elementoHTML)
     elemento.classList.add(classe)
     elemento.innerText = conteudo
@@ -14,17 +14,20 @@ function criarElemento(elementoHTML , classe, conteudo){
 }
  
 const cabecalhoTitulo = criarElemento("h1", "cabecalho__titulo", "Meu primeiro site utilizando JavaScript no Front-End")
-
 cabecalho.appendChild(cabecalhoTitulo)
-
 root.appendChild(cabecalho)
 
 //main 
-const principal = document.createElement("main")
-principal.classList.add("principal")
 
-const principalBotoes = document.createElement("ul")
-principalBotoes.classList.add("principal__botoes")
+
+const principal = criarElemento("main" , "principal")
+root.appendChild(principal)
+
+const sectionOpcoes = criarElemento("section", "section__Opcoes")
+principal.appendChild(sectionOpcoes)
+
+const principalBotoes = criarElemento("ul", "principal__botoes")
+sectionOpcoes.appendChild(principalBotoes)
 
 const principalBotoesLista = ["Quem sou", "Minhas Tecnologias", "Sites", "Rede Sociais"]
 
@@ -33,12 +36,22 @@ for (let i = 0; i < principalBotoesLista.length; i++) {
     principalBotoes.appendChild(botao)
 }
 
+const principalPerfil = criarElemento("section", "principal__perfil")
+principal.appendChild(principalPerfil)
+
+const imagemPerfil = criarElemento("img", "imagem__perfil" )
+imagemPerfil.src = "./imagem-perfil.jfif"
+principalPerfil.appendChild(imagemPerfil)
+
+const textoPerfil = criarElemento("p", "Luana Medeiros")
+principalPerfil.appendChild(textoPerfil)
 
 
-principal.appendChild(principalBotoes)
-root.appendChild(principal)
+// const principalFeed = document.createElement("section")
+// principalFeed.classList.add("principal__feed")
+// principal.appendChild(principalFeed)
 
-const principalFeed = document.createElement("section")
-principalFeed.classList.add("principal__feed")
 
-principal.appendChild(principalFeed)
+
+
+
