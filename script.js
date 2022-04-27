d7582b145cbe4e431519b48231c633ec23f1c885
luanafront <luana.lopes.rodrigues1@gmail.com>
2022-04-27 10:50:35 -0300

    function js

--- script.js
+++ script.js
@@ -4,12 +4,41 @@ console.log(root)
 const cabecalho = document.createElement("header")
 cabecalho.classList.add("cabecalho")
 
-const cabecalhoTitulo = document.createElement("h1")
-cabecalhoTitulo.classList.add("cabecalho__titulo")
-cabecalhoTitulo.innerText = "Meu primeiro site ultilizando JavaScript no Front-End"
+//funcao
+function criarElemento(elementoHTML , classe, conteudo){
+    const elemento = document.createElement(elementoHTML)
+    elemento.classList.add(classe)
+    elemento.innerText = conteudo
 
+    return elemento
+}
+ 
+const cabecalhoTitulo = criarElemento("h1", "cabecalho__titulo", "Meu primeiro site utilizando JavaScript no Front-End")
 
 cabecalho.appendChild(cabecalhoTitulo)
 
 root.appendChild(cabecalho)
 
+//main 
+const principal = document.createElement("main")
+principal.classList.add("principal")
+
+const principalBotoes = document.createElement("ul")
+principalBotoes.classList.add("principal__botoes")
+
+const principalBotoesLista = ["Quem sou", "Minhas Tecnologias", "Sites", "Rede Sociais"]
+
+for (let i = 0; i < principalBotoesLista.length; i++) {
+    let botao = criarElemento("li","principal__botoes--botao", principalBotoesLista[i])
+    principalBotoes.appendChild(botao)
+}
+
+
+
+principal.appendChild(principalBotoes)
+root.appendChild(principal)
+
+const principalFeed = document.createElement("section")
+principalFeed.classList.add("principal__feed")
+
+principal.appendChild(principalFeed)
